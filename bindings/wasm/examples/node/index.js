4187eb94afd79ebdd73cd7b01001a9157982f803
Mik <mik@opla.ai>
2020-01-22 17:30:34 +0100

    feat: bench base working

--- bindings/wasm/examples/node/index.js
+++ bindings/wasm/examples/node/index.js
@@ -1,9 +1,16 @@
-import { greet, build } from '../../pkg/wasm_tokenizers.js';
+import fs from 'fs';
+import { start, bench } from '../../pkg/wasm_tokenizers.js';
+
+const fsp = fs.promises;
 
 global.alert = (str) => {
   console.log(str);
 };
 
-greet('Mik');
-build("/Users/mik/dev/mikbry/tokenizers/bindings/wasm/examples/data/gpt2-vocab.json",
-  "/Users/mik/dev/mikbry/tokenizers/bindings/wasm/examples/data/gpt2-merges.txt");
+(async () => {
+  start('Mik');
+  const vocab = await fsp.readFile("../data/gpt2-vocab.json");
+  const merges = await fsp.readFile("../data/gpt2-merges.txt");
+  const inputs = await fsp.readFile("../data/big.txt");
+  bench(vocab, merges, inputs);
+})();
--- bindings/wasm/src/lib.rs
+++ bindings/wasm/src/lib.rs
@@ -1,10 +1,53 @@
-mod utils;
-use rayon::prelude::*;
-use std::collections::HashMap;
+use serde_json::Value;
+use std::error;
+use std::fmt;
+use std::{
+  collections::HashMap,
+  io,
+  io::BufRead,
+};
 use tokenizers::models::bpe::BPE;
 use tokenizers::pre_tokenizers::byte_level::ByteLevel;
 use tokenizers::tokenizer::{AddedToken, EncodeInput, Tokenizer};
 use wasm_bindgen::prelude::*;
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+  /// An error encountered while reading files mainly.
+  Io(std::io::Error),
+  JsonError(serde_json::Error),
+  BuildError,
+}
+
+impl From<io::Error> for Error {
+  fn from(error: io::Error) -> Self {
+      Error::Io(error)
+  }
+}
+
+impl From<serde_json::Error> for Error {
+  fn from(error: serde_json::Error) -> Self {
+      Error::JsonError(error)
+  }
+}
+
+impl fmt::Display for Error {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "build error")
+  }
+}
+
+impl error::Error for Error {
+  fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+    match self {
+      Error::Io(e) => Some(e),
+      Error::JsonError(e) => Some(e),
+      _ => None,
+    }
+  }
+}
 
 // Not public in Tokenizers
 type Pair = (u32, u32);
@@ -21,7 +64,7 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
+pub fn start(name: &str) {
   alert(&format!("Hello, {}!", name));
 }
 
@@ -42,30 +85,61 @@ fn create_gpt2_tokenizer(bpe: BPE) -> Tokenizer {
   tokenizer
 }
 
-#[wasm_bindgen]
-pub fn build(vocab_src: &str, merge_src: &str) {
-  alert("Build starting !");
+fn line_to_input(line: io::Result<String>) -> EncodeInput {
+  EncodeInput::Single(line.unwrap())
+}
+
+fn build(vocab_src: &[u8], merges_src: &[u8]) -> Result<Tokenizer> {
   let mut vocab = HashMap::<String, u32>::new();
-  vocab.insert("!".to_string(), 0);
-	vocab.insert("\"".to_string(), 1);
-	vocab.insert("#".to_string(), 2);
-	vocab.insert("$".to_string(), 3);
-	vocab.insert("%".to_string(), 4);
-	vocab.insert("&".to_string(), 5);
-	vocab.insert("'".to_string(), 6);
-	vocab.insert("(".to_string(), 7);
-  let mut merge = HashMap::<Pair, (u32, u32)>::new();
-  // let bpe = BPE::from_files(vocab, merge).unwrap().build().unwrap();
-  let bpe = BPE::new(vocab, merge);
+  let json: Value = serde_json::from_slice(&vocab_src).unwrap();
+  match json {
+    Value::Object(m) => {
+      for (token, id) in m {
+        if let Value::Number(id) = id {
+          let id = id.as_u64().ok_or(Error::BuildError)? as u32;
+          vocab.insert(token, id);
+        }
+      }
+    }
+    _ => return Err(Error::BuildError),
+  };
+  alert(&format!("vocab.len={}", vocab.len()));
+
+  let mut merges = HashMap::<Pair, (u32, u32)>::new();
+  //let merges_src = BufReader::new(merges_src);
+  for (rank, line) in merges_src.lines().enumerate() {
+    let line = line?;
+    if line.starts_with("#version") {
+      // Skip line with: #version
+      continue;
+    }
+
+    let parts = line.split(' ').collect::<Vec<_>>();
+    if parts.len() != 2 {
+      return Err(Error::BuildError);
+    }
+
+    let a = vocab.get(parts[0]).ok_or_else(|| Error::BuildError)?;
+    let b = vocab.get(parts[1]).ok_or_else(|| Error::BuildError)?;
+    let pair = (*a, *b);
+    let new_token = format!("{}{}", parts[0], parts[1]);
+    let new_id = vocab.get(&new_token).ok_or(Error::BuildError)?;
+
+    merges.insert(pair, (rank as u32, *new_id));
+  }
+  alert(&format!("merges.len={}", merges.len()));
+
+  let bpe = BPE::new(vocab, merges);
   let tokenizer = create_gpt2_tokenizer(bpe);
-  /* builder
-  .dropout(0.1)
-  .unk_token("[UNK]".into())
-  .build(); */
+  Ok(tokenizer)
+}
 
-  // let mut tokenizer = Tokenizer::new(Box::new(builder));
+#[wasm_bindgen]
+pub fn bench(vocab: &[u8], merges: &[u8], _inputs: &[u8]) {
+  alert("Bench starting !");
+  let tokenizer = build(vocab, merges).unwrap();
 
-  // let encoding = tokenizer.encode(EncodeInput::Single("Hey there!".into()));
-  // alert(encoding.get_tokens());
-  alert("Build done !");
+  let encoding = tokenizer.encode(EncodeInput::Single("Hey there!".into())).unwrap();
+  alert(&format!("tokens: {:?}", encoding.get_tokens()));
+  alert("Bench done !");
 }
